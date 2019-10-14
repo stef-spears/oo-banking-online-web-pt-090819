5e24c58181c110c4af023d25ff17ca418edbcb98
Stef Spears <spears.sj87@gmail.com>
2019-10-14 00:00:38 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,14 +2,11 @@ class BankAccount
   
   attr_reader :name
   attr_accessor :balance, :status
-  
-  @@all = []
 
   def initialize(name)
     @name = name
     @balance = 1000
     @status = "open"
-    @all << self 
   end
   
   def deposit(amount)
